0baa5d8a4aae20eb8ed626130afaef78b1b152a3
Luca1996 <lucagiuffrida1996@gmail.com>
2018-02-21 10:07:56 +0100

    added semclose

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,46 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+    // we first take the fd of the semaphore
+    // then we look for the semaphore
+
+    int fd = running->syscall_args[0];
+    SemDescriptor* sem_desc = SemDescriptorList_byFd(&running->sem_descriptors,fd);
+
+    // checking semaphore descriptor, if not found we raise an error
+
+    if (!sem_desc) {
+        running->syscall_retvalue = DSOS_ESEMAPHORENOFD;
+        return;
+    }
+
+    //removing semdescriptor from the list
+    sem_desc = (SemDescriptor*) List_detach(&running->sem_descriptors,(ListItem*)sem_desc);
+
+    // checking sem descriptor
+
+    if (!sem_desc) {
+        running->syscall_retvalue = DSOS_ESEMAPHORECLOSE;
+        return;
+    }
+
+    // taking the semaphore from the descriptor
+
+    Semaphore* sem = sem_desc->semaphore;
+
+    // now we can remove the descriptorPtr
+
+    SemDescriptorPtr* sem_desc_ptr = (SemDescriptorPtr*) List_detach(&sem->descriptors,(ListItem*)sem_desc->ptr);
+    
+    // checking pointer
+
+    if (!sem_desc_ptr) {
+        running->syscall_retvalue = DSOS_ESEMAPHOREDESC;
+        return;
+    }
+
+    SemDescriptor_free(sem_desc);
+    SemDescriptorPtr_free(sem_desc_ptr);
+    running->syscall_retvalue = 0;
+    return;
 }
