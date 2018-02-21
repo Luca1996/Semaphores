43057d80438472b346a452e00c65942848dafaa4
Luca1996 <lucagiuffrida1996@gmail.com>
2018-02-21 14:19:44 +0100

    added sempost

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,36 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+    // taking sem id
+    int id = running->syscall_args[0];
+
+    SemDescriptor* sem_desc = SemDescriptorList_byFd(&running->sem_descriptors,id);
+
+    // error check
+
+    if (!sem_desc) {
+        running->syscall_retvalue = DSOS_ESEMAPHORENOFD;
+        return;
+    }
+
+    // taking sem to take his value
+    Semaphore* sem = sem_desc->semaphore;
+    
+    // checking value
+    if (sem->count <= 0) {
+        sem->count = 0;
+        // removing first one from the waiting list
+        List_detach(&waiting_list,(ListItem*)waiting_list.first);
+    }
+    // incrementing count
+    sem->count++;
+
+    // then we insert the process in waiting list
+    List_insert(&waiting_list,waiting_list.last,(ListItem*)running);
+
+    // we set process status on ready
+    running->status = Ready;
+
+    running->syscall_retvalue = 0;
+    return;
 }
